772cb60a5588b09c11b432883671c8b584d8fda1
Craig <craigjart@gmail.com>
2023-11-03 05:08:15 -0400

    Bouncing MVP created.

---
+++ source/scripts/bouncyballs.js
@@ -0,0 +1,220 @@
+import delay from "./timing.js";
+
+const gravity = 0.3;
+const windStrength = 0;
+const fps = 60;
+const ballRefreshRate = 1000/fps;
+
+class Vector2{
+  x = Number;
+  y = Number;
+
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+
+  Compare(other){
+    return this.x === other.x && this.y === other.y;
+  }
+
+  Randomize(min, max, dimensions=["x","y"]){
+    if (typeof min !== "number" || typeof max !== "number"){
+      throw Error("You must define a minimum and maximum value, they must be numbers");
+    }
+    if (min > max) {
+      throw Error("Your minimum value is lower than the maximum value");
+    }
+
+    if (dimensions.includes("x")){
+      this.x = Math.random() * ((max) - min + 1) + min;
+    }
+
+    if (dimensions.includes("y")){
+      this.y = Math.random() * ((max) - min + 1) + min;
+    }
+
+    return this
+  }
+
+  RandomizeY(min, max){
+    this.Randomize(min, max, ["y"]);
+    return this.y;
+  }
+
+  RandomizeX(min, max){
+    this.Randomize(min, max, ["x"]);
+    return this.x;
+  }
+}
+
+class Material{
+  materialName = String;
+  weight = Number;
+  bounciness = Number;
+  friction = Number;
+  
+  constructor(materialName, weight=0.5, bounciness=0.5, friction=0.01){
+    this.weight = weight;
+    this.bounciness = bounciness;
+    this.materialName = materialName;
+    this.friction = friction;
+  }
+}
+
+class Technology{
+  technologyName = String;
+  imagePath = String;
+  imageOffsets = Object;
+
+  constructor(technologyName, imagePath, imageOffsets=new Vector2(0, 0)){
+    this.technologyName = technologyName;
+    this.imagePath = imagePath;
+    this.imageOffsets = imageOffsets;
+  }
+}
+
+const technologies = [
+  new Technology("Javascript", "./source/images/javascript_logo.jpg"),
+  new Technology("Python", "./source/images/python_logo.png", new Vector2(0, 50)),
+  new Technology("CSharp", "./source/images/csharp_logo.png"),
+  new Technology("React", "./source/images/react_logo.jpg"),
+  new Technology("Django", "./source/images/django_logo.png"),
+  // new Technology("TailwindCSS", "./source/images/tailwindcss_logo.jpg"),
+  // new Technology("SASS", "./source/images/sass_logo.png"),
+  // new Technology("Docker", ""), 
+  // new Technology("Git", ""),
+  // new Technology("Bootstrap", ""),
+]
+
+
+class TechnologyBall{
+  technology = Technology;
+  rotationAmount = Number;
+  material = Material;
+  position = Vector2;
+  anchorOffset = Vector2;
+  element = Element;
+
+  constructor(technology, material, position){
+    this.technology = technology;
+    this.material = material;
+    this.position = position;
+    this.rotationAmount = 0;
+  }
+
+  DecideTrajectory(){
+    return 0;
+  }
+
+  CreateTechnologyGFX(borderRadius=100, imageSize=64){
+    if (borderRadius > 100){
+      throw Error("Value of borderRadius must be between 0-100");
+    }
+    
+    const technologyGFX = document.createElement("img");
+    technologyGFX.onclick = () => { console.log("You clicked me!!"); }
+  
+    technologyGFX.src = this.technology.imagePath;
+    technologyGFX.alt = this.technology.technologyName + " Icon";
+  
+    technologyGFX.style.cssText = `
+      border-radius: ${borderRadius}%;
+      height: ${imageSize}px;
+      width: ${imageSize}px;
+      position: absolute;
+      bottom: ${this.position.y}%;
+      right: ${this.position.x}%;
+      object-fit: cover;
+      object-position: ${this.technology.imageOffsets.y}% ${this.technology.imageOffsets.x}%;
+      transform: translate(50%,50%);
+    `;
+    
+    this.element = technologyGFX;
+    this.anchorOffset = new Vector2(
+      parseInt(this.element.style.width)/2,
+      parseInt(this.element.style.height)/2
+    );
+
+    return technologyGFX;
+  }
+
+
+  async UpdatePosition(element, appliedForce){
+    if(document.visibilityState === "visible"){
+      if (!this.velocity){
+        this.velocity = new Vector2(appliedForce.x, appliedForce.y);
+      }
+
+      this.position.y += appliedForce.y;
+      this.position.x += appliedForce.x;
+      appliedForce.x -= windStrength / this.material.weight;
+      appliedForce.y -= gravity * this.material.weight;
+
+      if (this.position.y <= 0){
+        this.position.y = 0;
+        appliedForce.y = this.velocity.y * this.material.bounciness;
+        this.velocity.y = appliedForce.y;
+
+
+        if (appliedForce.x > 0){
+          if (appliedForce.x - this.material.friction < 0){
+            appliedForce.x = 0;
+          } else {
+            appliedForce.x -= this.material.friction;
+          }
+        } else if (appliedForce.x > 0) {
+          if (appliedForce.x + this.material.friction < 0){
+            appliedForce.x = 0;
+          } else {
+            appliedForce.x += this.material.friction;
+          }
+        }
+      }
+
+      if(appliedForce.x <= 0 && windStrength === 0){
+        element.parentNode.removeChild(element);
+        return 0;
+      }
+
+      if(this.position.x >= 100 || this.position.x <= -20){
+        element.parentNode.removeChild(element);
+        return 0
+      }
+
+      element.style.right = `calc(${this.position.x}% + ${this.anchorOffset.x}px)`;
+      element.style.bottom = `calc(${this.position.y}% + ${this.anchorOffset.y}px)`;
+    }
+
+    await delay(ballRefreshRate);
+    this.UpdatePosition(element, appliedForce);
+  }
+}
+
+const materials = [
+  new Material("silly name", 0.3, 0.7, 0),
+  new Material("silly name", 0.47, 0.03, 0.01),
+  new Material("silly name", 0.81, 1, 0.01),
+  new Material("silly name", 0.29, 0.69, 0)
+]
+
+export default async function StartBouncing(parent){
+  if (document.visibilityState === "visible"){
+    let randomTechnologyIndex = Math.floor(Math.random() * ((technologies.length - 1) - 0 + 1)) + 0
+    let randomMaterialIndex = Math.floor(Math.random() * ((materials.length - 1) - 0 + 1)) + 0
+    const randomMaterial = materials[randomMaterialIndex];
+    const randomTechnology = technologies[randomTechnologyIndex];
+    const randomForce = new Vector2(0,0)
+    randomForce.RandomizeX(0.1,2);
+    randomForce.RandomizeY(0.2,5);
+
+    const TechBall = new TechnologyBall(randomTechnology, randomMaterial, new Vector2(-10, -10));
+
+    TechBall.CreateTechnologyGFX();
+    parent.appendChild(TechBall.element);
+    TechBall.UpdatePosition(TechBall.element, new Vector2(.5, 5), 10);
+  }
+
+  await delay(500);
+  StartBouncing(parent);
+}
