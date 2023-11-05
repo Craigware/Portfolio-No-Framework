f8bf0bea2c500772585b8477b8ca8bfb41a408c6
Craig <craigjart@gmail.com>
2023-11-04 23:10:26 -0400

    Working on css a bit, gonna merge with what i did on my laptop.

--- portfolio.js
+++ portfolio.js
@@ -16,31 +16,31 @@ const alterWorldSettingsForm = document.getElementById("World-Settings");
 alterWorldSettingsForm.addEventListener("change", AlterWorldEffects);
 
 
-function GenerateHeroGrid(){
-  const gridContainer = document.getElementById("Hero");
-  const gridSize = 25;
-  for (let i = 0; i < gridSize + 1; i++){
-    const gridLine = document.createElement("div")
-    gridLine.classList.add("gridLine");
-    gridContainer.appendChild(gridLine);
-    gridLine.style.width = "1px";
-    gridLine.style.height = "100%";
-    gridLine.style.position = "absolute";
-    gridLine.style.backgroundColor = "white";
-    gridLine.style.left = `${i * 100/gridSize}%`
-    gridLine.style.zIndex = "0"
-  }
+// function GenerateHeroGrid(){
+//   const gridContainer = document.getElementById("Hero");
+//   const gridSize = 25;
+//   for (let i = 0; i < gridSize + 1; i++){
+//     const gridLine = document.createElement("div")
+//     gridLine.classList.add("gridLine");
+//     gridContainer.appendChild(gridLine);
+//     gridLine.style.width = "1px";
+//     gridLine.style.height = "100%";
+//     gridLine.style.position = "absolute";
+//     gridLine.style.backgroundColor = "white";
+//     gridLine.style.left = `${i * 100/gridSize}%`
+//     gridLine.style.zIndex = "0"
+//   }
 
-  for (let i = 0; i < gridSize + 1; i++){
-    const gridLine = document.createElement("div")
-    gridLine.classList.add("gridLine");
-    gridContainer.appendChild(gridLine);
-    gridLine.style.height = "1px";
-    gridLine.style.width = "100%";
-    gridLine.style.position = "absolute";
-    gridLine.style.backgroundColor = "white";
-    gridLine.style.top = `${i * 156.25/gridSize}%`
-    gridLine.style.zIndex = "0"
-  }
-}
-GenerateHeroGrid();
+//   for (let i = 0; i < gridSize + 1; i++){
+//     const gridLine = document.createElement("div")
+//     gridLine.classList.add("gridLine");
+//     gridContainer.appendChild(gridLine);
+//     gridLine.style.height = "1px";
+//     gridLine.style.width = "100%";
+//     gridLine.style.position = "absolute";
+//     gridLine.style.backgroundColor = "white";
+//     gridLine.style.top = `${i * 156.25/gridSize}%`
+//     gridLine.style.zIndex = "0"
+//   }
+// }
+// GenerateHeroGrid();
