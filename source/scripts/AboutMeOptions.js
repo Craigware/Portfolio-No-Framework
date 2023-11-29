2bb9a60f8ad12206517563624295f7d83586342e
Craig <craigjart@gmail.com>
2023-11-29 18:54:41 -0500

    Created seperate file for option select in about me section.

---
+++ source/scripts/AboutMeOptions.js
@@ -0,0 +1,36 @@
+async function getComponentByFile(filePath){
+  return fetch(filePath).then((res) => (res.text()));
+}
+
+const aboutMe = document.getElementById("About-Me");
+const hobbyOption = aboutMe.getElementsByClassName("option");
+
+function ChangeContents(event, section){
+  let content = {};
+  if (section === document.getElementById("About-Me")){
+    content = {
+      "default": defaultContents,
+      "hobbies": hobbyContents,
+      "goals": goalsContents,
+      "soft-skills": softSkillsContents,
+      "dog-pictures": "3",
+      "doodle-gallery": "4"
+    };
+  }
+  
+
+  const sectionContent = section.getElementsByClassName("content")[0];
+  event.target.value = event.target.getAttribute("value");
+  sectionContent.innerHTML = content[event.target.value];
+}
+
+for (let i = 0; i < hobbyOption.length; i++){
+  hobbyOption[i].addEventListener("click", (e) => {ChangeContents(e, aboutMe)});
+}
+
+window.ChangeContents = ChangeContents;
+
+const defaultContents = aboutMe.getElementsByClassName("content")[0].innerHTML;
+const hobbyContents = await getComponentByFile("./source/components/aboutme/HobbyComponent.html");
+const goalsContents = await getComponentByFile("./source/components/aboutme/GoalsComponent.html");
+const softSkillsContents = await getComponentByFile("./source/components/aboutme/SoftSkillsComponent.html");
